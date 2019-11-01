9b7dd69acfa8a339f1880410a291997b2e126462
jpbp <jpbp@estudante.ufla.br>
2019-11-01 16:00:46 -0300

    recorte

---
+++ recorte/clip-rectangle.cpp
@@ -0,0 +1,43 @@
+#include "clip-rectangle.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+ClipRectangle::ClipRectangle (double xmin, double xmax, double ymin, double ymax)
+    : mXmin(xmin), mXmax(xmax), mYmin(ymin), mYmax(ymax), mInitialized(true)
+{}
+
+void ClipRectangle::Set(double xmin, double xmax, double ymin, double ymax)
+{
+    mXmin = xmin;
+    mXmax = xmax;
+    mYmin = ymin;
+    mYmax = ymax;
+    mInitialized = true;
+}
+
+// Retorna um ponteiro para uma linha recem alocada inteiramente dentro da area de recorte
+// ou NULL se l for completamente descartada.
+// A linha alocada aqui será desalocada na função CleanUp().
+Line* ClipRectangle::Clip(const Line& l)
+{
+    int t=0;
+   l.Coordinates(t);
+}
+
+void ClipRectangle::Read(const string& prompt)
+{
+    cout << prompt;
+    cin >> mXmin >> mXmax >> mYmin >> mYmax;
+    if (not mInitialized)
+        mInitialized = true;
+}
+
+ostream& operator << (ostream& os, const ClipRectangle& r)
+{
+    return os << "(Clipping Rectangle: " << r.mXmin << ", " << r.mXmax << ", " 
+              << r.mYmin << ", " << r.mYmax << ")";
+}
+
+
