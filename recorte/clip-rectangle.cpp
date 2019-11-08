89d1a04c5df5f2332a77f9c9ab384a70169bf5c9
jpbp <jpbp@estudante.ufla.br>
2019-11-08 16:30:19 -0300

    recorte esquerdo

--- recorte/clip-rectangle.cpp
+++ recorte/clip-rectangle.cpp
@@ -22,16 +22,64 @@ void ClipRectangle::Set(double xmin, double xmax, double ymin, double ymax)
 // A linha alocada aqui será desalocada na função CleanUp().
 Line* ClipRectangle::Clip(const Line& l)
 {
+    Point newPoint;
+    Line *linha= new Line;
     double inicio=0;
     double fim =1;
+    double numerador;
+    double denominador;
+    double t=0;
+    cout<<l<<endl;
     for (size_t i = 0; i < 4; i++)
     {
         if(i==0){ // lado esquerdo 
-            double numerador=mXmin-l.mP0.mX;
-            double denominador= l.mP1.mX-l.mP0.mX;
+            numerador=mXmin-l.mP0.mX;
+            denominador= l.mP1.mX-l.mP0.mX;
+            if(denominador==0){// segmento paralelo a area de recorte
+                if(numerador>0){ // fora da area de recorte
+                    // 
+                return NULL;
+                }
+            }
+            else{
+                t=numerador/denominador;
+                cout<<denominador<<endl;
+                if(denominador>0){
+                    if(t>fim){
+                        
+                        return NULL;
+                    }
+                    else if(t>inicio){
+                        inicio=t;
+                    }
+                }
+                else{
+                    if(t<inicio){
+                        return NULL;
+                    }
+                    else if(t>fim){
+                        fim=t;
+                    }
+                }
+            }
         }
+                
+        
+        
     }
-    
+    if(inicio!=0){
+            newPoint= l.mP0+(l.mP1-l.mP0)*inicio;
+            linha->mP0=newPoint;
+            linha->mP1=l.mP1;
+        }
+        if(fim!=1){
+            newPoint= l.mP0+(l.mP1-l.mP0)*fim;
+            linha->mP0=l.mP0;
+            linha->mP1=newPoint;
+        }
+    cout<<linha->mP0.mX;
+    cout<<linha->mP0.mY;
+    return linha;
 }
 
 void ClipRectangle::Read(const string& prompt)
