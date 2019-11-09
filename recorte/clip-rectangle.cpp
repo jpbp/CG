66865dad5c2314c72c64e9073330a51853b831e5
jpbp <jpbp@estudante.ufla.br>
2019-11-09 19:28:55 -0300

    recorte feito

--- recorte/clip-rectangle.cpp
+++ recorte/clip-rectangle.cpp
@@ -21,17 +21,59 @@ void ClipRectangle::Set(double xmin, double xmax, double ymin, double ymax)
 // ou NULL se l for completamente descartada.
 // A linha alocada aqui será desalocada na função CleanUp().
 Line* ClipRectangle::Clip(const Line& l)
-{
-    double inicio=0;
-    double fim =1;
-    for (size_t i = 0; i < 4; i++)
-    {
-        if(i==0){ // lado esquerdo 
-            double numerador=mXmin-l.mP0.mX;
-            double denominador= l.mP1.mX-l.mP0.mX;
+{  
+   Line *linha = new Line;
+   Point ponto; 
+   double t;
+   double inicio=0;
+   double fim=1;
+   double numerador[]={mXmin - l.mP0.mX, l.mP0.mX - mXmax, l.mP0.mY - mYmax, mYmin-l.mP0.mY};
+   double denominador[]={l.mP1.mX - l.mP0.mX, l.mP0.mX - l.mP1.mX, l.mP0.mY - l.mP1.mY, l.mP1.mY-l.mP0.mY};
+   for (size_t i = 0; i < 4; i++)
+   {
+        if(denominador[i]==0){
+            if(numerador[i]>0){
+                return NULL;
+            }
         }
-    }
-    
+        else{
+            t=numerador[i]/denominador[i];
+            if(denominador[i]>0){
+                if(t>fim){
+                    return NULL;
+                }
+               if(t>inicio){
+                    inicio=t;
+                }
+            }
+            else{
+                if(t<inicio){
+                    return NULL;
+                }
+                if(t<fim){
+                    fim=t;
+                }
+            }
+        }    
+   }
+   if(inicio==0 and fim==1){
+       linha->mP0=l.mP0;
+       linha->mP1=l.mP1;
+       return linha;
+   }
+   if(inicio!=0){
+       cout<<"valor de t: "<<t<<" "<<"valor inicio e fim: "<<inicio<<" "<<fim<<endl;
+        ponto=l.mP0+(l.mP1-l.mP0)*inicio;
+        linha->mP0=ponto;
+        linha->mP1=l.mP1;
+   }
+   if(fim!=1){
+       cout<<"valor de t: "<<t<<" "<<"valor inicio e fim: "<<inicio<<" "<<fim<<endl;
+        ponto=l.mP0+(l.mP1-l.mP0)*fim;
+        //linha->mP0=l.mP0;
+        linha->mP1=ponto;
+   }
+   return linha;
 }
 
 void ClipRectangle::Read(const string& prompt)
